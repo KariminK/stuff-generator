9d45ce1c40b7979b57b123169c65a5400a4ad309
KariminK <103779342+KariminK@users.noreply.github.com>
2024-08-17 21:33:02 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Navbar from "./Navbar";
 
 
 function App() {
   return <div className="bg-slate-950 h-screen">
-    
+    <Navbar />
   </div>;
 }
 
---
+++ src/Navbar.tsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-950 py-10 text-3xl font-bold text-white">
+      <ul className="flex justify-around items-center max-w-5xl mx-auto">
+        <li>
+          <a className="underline" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <p className="text-center text-xl">
+            Generate <span className="text-4xl text-pink-200">Your</span> <br />{" "}
+            Stuff
+          </p>
+        </li>
+        <li>
+          <a className="underline" href="/about">
+            About
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
